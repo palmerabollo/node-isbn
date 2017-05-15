96b280c4c9a76ff03ddd0984c537069d9d54da1b
infojunkie <karim.ratib@gmail.com>
2017-05-15 13:53:03 -0700

    Remove Object.assign polyfill

--- index.js
+++ index.js
@@ -9,32 +9,6 @@ var defaultOptions = {
   timeout: 5000
 };
 
-// https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
-if (typeof Object.assign != 'function') {
-  Object.assign = function(target, varArgs) { // .length of function is 2
-    'use strict';
-    if (target == null) { // TypeError if undefined or null
-      throw new TypeError('Cannot convert undefined or null to object');
-    }
-
-    var to = Object(target);
-
-    for (var index = 1; index < arguments.length; index++) {
-      var nextSource = arguments[index];
-
-      if (nextSource != null) { // Skip over if undefined or null
-        for (var nextKey in nextSource) {
-          // Avoid bugs when hasOwnProperty is shadowed
-          if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
-            to[nextKey] = nextSource[nextKey];
-          }
-        }
-      }
-    }
-    return to;
-  };
-}
-
 var GOOGLE_BOOKS_API_BASE = 'https://www.googleapis.com';
 var GOOGLE_BOOKS_API_BOOK = '/books/v1/volumes';
 
